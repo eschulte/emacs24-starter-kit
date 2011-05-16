8dd4356ff58c704a0c5587a243bfcaaea5ef8e4f
Eric Schulte <schulte.eric@gmail.com>
2011-05-15 20:14:50 -0600

    updating init assuming Emacs24 with current Org-mode

--- init.el
+++ init.el
@@ -7,14 +7,7 @@
 
 (setq dotfiles-dir (file-name-directory (or load-file-name (buffer-file-name))))
 
-(add-to-list 'load-path (expand-file-name
-                         "lisp" (expand-file-name
-                                 "org" (expand-file-name
-                                        "src" dotfiles-dir))))
-;; Load up Org Mode and Babel
-(require 'org-install)
-
-;; load up the main file
+;; load up the starter kit
 (org-babel-load-file (expand-file-name "starter-kit.org" dotfiles-dir))
 
 ;;; init.el ends here
