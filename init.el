4e1d2456e2dd0763694d4106c2dc2620e45face5
Eric Schulte <eric.schulte@gmx.com>
2011-12-23 11:30:19 -0700

    including init.el

---
+++ init.el
@@ -0,0 +1,15 @@
+;;; init.el --- Where all the magic begins
+;;
+;; Part of the Emacs Starter Kit
+;;
+;; This is the first thing to get loaded.
+;;
+
+;; remember this directory
+(setq starter-kit-dir
+      (file-name-directory (or load-file-name (buffer-file-name))))
+
+;; load up the starter kit
+(org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))
+
+;;; init.el ends here
