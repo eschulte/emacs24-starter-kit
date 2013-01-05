2de9593f72c31ba84382f50271e9bf698a9ae995
Eric Schulte <schulte.eric@gmail.com>
2013-01-05 16:26:10 -0700

    use after-init-hook to load the starter kit

--- init.el
+++ init.el
@@ -5,11 +5,14 @@
 ;; This is the first thing to get loaded.
 ;;
 
-;; remember this directory
-(setq starter-kit-dir
-      (file-name-directory (or load-file-name (buffer-file-name))))
-
-;; load up the starter kit
-(org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))
+;; load the starter kit from the `after-init-hook' so all packages are loaded
+(add-hook 'after-init-hook
+ `(lambda ()
+    ;; remember this directory
+    (setq starter-kit-dir
+          ,(file-name-directory (or load-file-name (buffer-file-name))))
+    ;; load up the starter kit
+    (require 'org)
+    (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 ;;; init.el ends here
