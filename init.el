99107e9ee84c2935a614d7ac809aa27bce63c037
Eric Schulte <schulte.eric@gmail.com>
2013-04-24 13:48:38 -0600

    load Org from source if ORG_HOME env variable set
      If the user has defined the ORG_HOME environment variable, then we
      assume both that the user has installed Org-mode from source, and that
      this variable points to the base of the Org-mode install.  In this
      case we add the value of ORG_HOME to the load path, and load up
      Org-mode immediately.

--- init.el
+++ init.el
@@ -5,14 +5,25 @@
 ;; This is the first thing to get loaded.
 ;;
 
+;; load Org-mode from source when the ORG_HOME environment variable is set
+(when (getenv "ORG_HOME")
+  (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
+    (when (file-directory-p org-lisp-dir)
+      (add-to-list 'load-path org-lisp-dir)
+      (require 'org))))
+
 ;; load the starter kit from the `after-init-hook' so all packages are loaded
 (add-hook 'after-init-hook
  `(lambda ()
     ;; remember this directory
     (setq starter-kit-dir
           ,(file-name-directory (or load-file-name (buffer-file-name))))
+    ;; only load org-mode later if we didn't load it just now
+    ,(unless (and (getenv "ORG_HOME")
+                  (file-directory-p (expand-file-name "lisp"
+                                                      (getenv "ORG_HOME"))))
+       '(require 'org))
     ;; load up the starter kit
-    (require 'org)
     (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 ;;; init.el ends here
